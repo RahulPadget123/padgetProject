751af18ab3bc04f0907425ff1e2eba1a8f133945
RahulPadget123 <rahulraj.padget68@dixoninfo.com>
2025-03-28 15:07:20 +0530

    Update dbConfig.js

--- dbConfig.js
+++ dbConfig.js
@@ -1,18 +1,7 @@
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
-  try {
-    const mongoURI = process.env.MONGO_URI || 'your-mongodb-connection-string';
-    await mongoose.connect(mongoURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 30000, // Increase timeout to 30 seconds
-    });
-    console.log('MongoDB connected successfully');
-  } catch (err) {
-    console.error('MongoDB connection error:', err.message);
-    process.exit(1); // Exit process with failure
-  }
-};
+async function connectMongoDB(url){
+    return mongoose.connect(url);
+}
 
-module.exports = connectDB;
+module.exports = connectMongoDB;
